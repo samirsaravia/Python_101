59826a196b6a7c6a9e966ffd011efb222ceff5ae
Samir <samir.saravia.10@gmail.com>
2020-04-21 18:33:53 -0300

    alistamento militar
    Signed-off-by: Samir <samir.saravia.10@gmail.com>

--- Python3-world2/ex0039.py
+++ Python3-world2/ex0039.py
@@ -5,10 +5,10 @@ idade = data_atual - data_nascimento
 if idade < 16:
     print('Ainda vai se alistar ao seriço militar.')
     print('Faltam {} ano/s'.format(17 - idade))
-    print('Você vai se alistar no ano {}-{}'.format(data + 17, data + 18))
+    print('Você vai se alistar no ano {}-{}'.format(data_nascimento + 17, data_nascimento+ 18))
 elif 16 < idade <= 18:
     print('Você está na hora de se alistar.')
 else:
     print('Você ja passou do tempo do alistamento.')
     print('com {} ano/s.'.format(idade - 18))
-    print('Você tinha que se alistar no ano {} - {}'.format(data + 17, data + 18))
+    print('Você tinha que se alistar no ano {} - {}'.format(data_nascimento + 17, data_nascimento + 18))
