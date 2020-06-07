74f5c29a8768042c20f45213d647a16a9f66fcd9
Samir <samir.saravia.10@gmail.com>
2020-06-07 17:12:23 -0300

    for-loop and while-loop

---
+++ Python for beginners-Mdev/for_while.py
@@ -0,0 +1,10 @@
+people = ['Samir', 'Susan', 'Christopher', 'Rupert']
+# first way to check a list
+for name in people:
+    print(name)
+# second way to check a list
+index = 0
+while index < len(people):
+    print(people[index])
+    index += 1
+# both have the same effect
