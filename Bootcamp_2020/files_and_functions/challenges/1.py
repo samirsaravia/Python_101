7f7be65508a72489cfc3be69d891dcbea8f9b0e5
Samir <samir.saravia.10@gmail.com>
2020-07-05 17:27:24 -0300

    Summing total

---
+++ Bootcamp_2020/files_and_functions/challenges/1.py
@@ -0,0 +1,16 @@
+"""
+Question 1
+Create a function that will calculate the sum of two numbers.
+Call it sum_two
+"""
+
+
+def sum_two(num_1, num_2):
+    """
+    This function returns the sum of two numbers
+    """
+    return num_1 + num_2
+
+
+print(f'The sum of 45 and 21 is {sum_two(45, 21)}')
+help(sum_two)
