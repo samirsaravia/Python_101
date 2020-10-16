bee7bd2d6db2f69d79f54fdacd2d6f776c6bb0d6
Samir <samir.saravia.10@gmail.com>
2020-10-16 18:49:58 -0300

    little bugs

--- py_wha/clock.py
+++ py_wha/clock.py
@@ -1,10 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+from honey_love import send_love
 
 
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(job_function, 'interval', seconds=10)
+sched.add_job(send_love, 'interval', seconds=10)
 
 sched.start()
